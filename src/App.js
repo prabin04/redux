5d774116f7eaafdc6306f64ddaddeb5397c102c2
Prabin Parajuli <prabin.b9@gmail.com>
2023-02-20 15:17:34 +0100

    UPDATED useReducer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { useReducer} from 'react'
 import './App.css';
 
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
+}
+
 function reducer(state, action){
   switch(action.type){
-    case 'increment':
+    case ACTIONS.INCREMENT:
       return {count: state.count +1}
-    case 'decrement':
+    case ACTIONS.DECREMENT:
       return{count: state.count - 1}
     default:
       return state
@@ -17,11 +22,11 @@ function App() {
   const [state, dispatch] = useReducer(reducer, {count: 0})
 
   function increment(){
-    dispatch({type: 'increment'})
+    dispatch({type: ACTIONS.INCREMENT})
   }
 
   function decrement(){
-    dispatch({type: 'decrement'})
+    dispatch({type: ACTIONS.DECREMENT})
   }
 
   return (
